b2b46b896f6ac03924393b2563f4071a7d710cd5
Chris Norman <cnorman@broadinstitute.org>
2017-01-18 11:23:24 -0500

    Change internal command line exception classes to package-protected and provide a clean separation from user exceptions.

--- src/main/java/org/broadinstitute/barclay/argparser/CommandLineException.java
+++ src/main/java/org/broadinstitute/barclay/argparser/CommandLineException.java
@@ -76,8 +76,9 @@ public class CommandLineException extends RuntimeException {
      * Class CommandLineParserInternalException
      * <p/>
      * For internal errors in the command line parser not related to syntax errors in the command line itself.
+     * Package protected to restrict usage to the arg parsers.
      */
-    public static class CommandLineParserInternalException extends CommandLineException {
+    static class CommandLineParserInternalException extends RuntimeException {
         private static final long serialVersionUID = 0L;
         public CommandLineParserInternalException( final String s ) {
             super(s);
@@ -90,8 +91,9 @@ public class CommandLineException extends RuntimeException {
 
     /**
      * For wrapping errors that are believed to never be reachable
+     * Package protected to restrict usage to the arg parsers.
      */
-    public static class ShouldNeverReachHereException extends CommandLineException {
+    static class ShouldNeverReachHereException extends RuntimeException {
         private static final long serialVersionUID = 0L;
         public ShouldNeverReachHereException( final String s ) {
             super(s);
@@ -99,7 +101,7 @@ public class CommandLineException extends RuntimeException {
         public ShouldNeverReachHereException( final String s, final Throwable throwable ) {
             super(s, throwable);
         }
-        public ShouldNeverReachHereException( final Throwable throwable) {this("Should never reach here.", throwable);}
+        public ShouldNeverReachHereException( final Throwable throwable) {this("Barclay command line parser; should never reach here.", throwable);}
     }
 
 }
