560dfa20363eb6a99b79ae658ca6053c0b26f37e
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-05-26 12:04:41 +0200

    Repackage generic test templates, remove supercat and test default Doclet

--- src/test/java/org/broadinstitute/barclay/help/DocumentationGenerationIntegrationTest.java
+++ src/test/java/org/broadinstitute/barclay/help/DocumentationGenerationIntegrationTest.java
@@ -65,4 +65,44 @@ public class DocumentationGenerationIntegrationTest {
         return Arrays.equals(actualBytes, expectedBytes);
     }
 
+    @Test
+    public void testDefaultDoclet() throws IOException {
+        File outputDir = Files.createTempDirectory("BarclayDocGen").toAbsolutePath().toFile();
+        File expectedDir = new File("src/test/resources/org/broadinstitute/barclay/help/expected/default");
+
+        outputDir.deleteOnExit();
+
+        String[] argArray = new String[]{
+                "-build-timestamp", "2016/01/01 01:01:01",      // dummy, constant timestamp
+                "-absolute-version", "11.1",                    // dummy version
+                "-settings-dir", "src/main/resources/org/broadinstitute/barclay/helpTemplates/",
+                "-d", outputDir.getAbsolutePath(),
+                "-output-file-extension", "html",
+                "-doclet", TestDoclet.class.getName(),
+                "-docletpath", "build/libs",
+                "-sourcepath", "src/test/java",
+                "org.broadinstitute.barclay.help",
+                "org.broadinstitute.barclay.argparser",
+                "-verbose",
+                "-cp", System.getProperty("java.class.path")
+        };
+
+        List<String> docArgList = new ArrayList<>();
+        docArgList.addAll(Arrays.asList(argArray));
+
+        // run javadoc with the the custom doclet
+        com.sun.tools.javadoc.Main.execute(docArgList.toArray(new String[]{}));
+
+        // Compare output files
+        Assert.assertTrue(filesContentsIdentical(outputDir, expectedDir, "index.html"));
+        Assert.assertTrue(filesContentsIdentical(outputDir, expectedDir,
+                "org_broadinstitute_barclay_help_TestArgumentContainer.html"));
+        Assert.assertTrue(filesContentsIdentical(outputDir, expectedDir,
+                "org_broadinstitute_barclay_help_TestArgumentContainer.html.json"));
+        Assert.assertTrue(filesContentsIdentical(outputDir, expectedDir,
+                "org_broadinstitute_barclay_help_TestExtraDocs.html"));
+        Assert.assertTrue(filesContentsIdentical(outputDir, expectedDir,
+                "org_broadinstitute_barclay_help_TestExtraDocs.html.json"));
+    }
+
 }
