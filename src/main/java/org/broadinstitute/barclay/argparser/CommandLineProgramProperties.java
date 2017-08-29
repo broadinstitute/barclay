46af7847b029b42dcff88aab8323c6a4a8ffc80f
Chris Norman <cnorman@broadinstitute.org>
2017-08-29 15:34:44 -0400

    Update CommandLineProgramProperties interface documentation, including recommended use of omitFromCommandLine.

--- src/main/java/org/broadinstitute/barclay/argparser/CommandLineProgramProperties.java
+++ src/main/java/org/broadinstitute/barclay/argparser/CommandLineProgramProperties.java
@@ -25,7 +25,22 @@ public @interface CommandLineProgramProperties {
      * @return an example command line for this program
      */
     String usageExample() default "The author of this program hasn't included any example usage, please complain to them.";
+
+    /**
+     * Assign this command line program to an (application-defined) program group for use in managing help output
+     * and documentation.
+     * @return CommandLineProgramGroup for this command line program
+     */
     Class<? extends CommandLineProgramGroup> programGroup();
 
+    /**
+     * Property for use by client applications that may want to hide some commandline programs from interactive
+     * command line help output. This property is for use by consuming applications only. Barclay does not use
+     * or enforce this property for any purpose (including generated documentation, which is governed separately by
+     * the {@link org.broadinstitute.barclay.help.DocumentedFeature} annotation).
+     *
+     * @return {@code true} if this command line program should be omitted by the Barclay client application from
+     * interactive help output, otherwise {@code false}
+     */
     boolean omitFromCommandLine() default false;
 }
