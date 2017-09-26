e2320daa107f0b0156f2c29e4ca93dd778b72b1b
Chris Norman <cnorman@broadinstitute.org>
2017-09-25 20:28:16 -0400

    Update CommandLineProgramProperties interface documentation, including recommended use of omitFromCommandLine. (#93)

--- src/main/java/org/broadinstitute/barclay/argparser/CommandLineProgramGroup.java
+++ src/main/java/org/broadinstitute/barclay/argparser/CommandLineProgramGroup.java
@@ -3,15 +3,18 @@ package org.broadinstitute.barclay.argparser;
 import java.util.Comparator;
 
 /**
- * Interface for groups of CommandLinePrograms.
+ * Annotation interface to allow a command line program to be assigned to an (application-defined) program group for
+ * use in managing help output and documentation.
  * @author Nils Homer
  */
 public interface CommandLineProgramGroup {
 
     /** Gets the name of this group. **/
     public String getName();
+
     /** Gets the description of this group. **/
     public String getDescription();
+
     /** Compares two program groups by name. **/
     public static Comparator<CommandLineProgramGroup> comparator = (a, b) -> a.getName().compareTo(b.getName());
 }
