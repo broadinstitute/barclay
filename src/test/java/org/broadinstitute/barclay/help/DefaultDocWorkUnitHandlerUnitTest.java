d1eb2d32d290b105d55575932cab4d83a3ca353c
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2017-10-06 18:49:35 +0200

    Address comments 2

--- src/test/java/org/broadinstitute/barclay/help/DefaultDocWorkUnitHandlerUnitTest.java
+++ src/test/java/org/broadinstitute/barclay/help/DefaultDocWorkUnitHandlerUnitTest.java
@@ -1,5 +1,6 @@
 package org.broadinstitute.barclay.help;
 
+import com.sun.javadoc.ClassDoc;
 import org.broadinstitute.barclay.argparser.TestProgramGroup;
 import org.broadinstitute.barclay.help.testinputs.TestArgumentContainer;
 import org.broadinstitute.barclay.help.testinputs.TestExtraDocs;
@@ -8,6 +9,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author Daniel Gomez-Sanchez (magicDGS)
@@ -17,72 +19,71 @@ public class DefaultDocWorkUnitHandlerUnitTest {
     @DataProvider
     public static Object[][] workUnitDescriptions() {
         return new Object[][] {
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestArgumentContainer.class),
-                        ""},
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestExtraDocs.class, DocGenMocks.mockClassDoc("Javadoc description")),
-                        ""},
-                // because DefaultWorkUnitHandler does not define an extra tag, the mocked class doc uses an empty tag name for the inline tag
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestExtraDocs.class, DocGenMocks
-                        .mockClassDoc("Javadoc description \nin two lines", Collections.singletonMap("", "Inline tag"))),
-                        "Inline tag"}
+                {TestArgumentContainer.class, "", Collections.EMPTY_MAP, ""},
+                {TestExtraDocs.class, "Javadoc description", Collections.EMPTY_MAP, ""},
+                {TestExtraDocs.class, "Javadoc description \nin two lines", Collections.singletonMap("", "Inline tag"), "Inline tag"}
         };
     }
 
     @DataProvider
     public static Object[][] workUnitSummary() {
         return new Object[][] {
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestArgumentContainer.class),
-                        TestArgumentContainer.ONE_LINE_SUMMARY},
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestExtraDocs.class, DocGenMocks.mockClassDoc("Javadoc description")),
-                        "Javadoc description"},
-                // because DefaultWorkUnitHandler does not define an extra tag, the mocked class doc uses an empty tag name for the inline tag
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestExtraDocs.class, DocGenMocks
-                        .mockClassDoc("Javadoc description \nin two lines", Collections.singletonMap("", "Inline tag"))),
-                        "Javadoc description in two lines"}
+                {TestArgumentContainer.class, "", Collections.EMPTY_MAP, TestArgumentContainer.ONE_LINE_SUMMARY},
+                {TestExtraDocs.class, "Javadoc description", Collections.EMPTY_MAP, "Javadoc description"},
+                {TestExtraDocs.class, "Javadoc description \nin two lines", Collections.singletonMap("", "Inline tag"), "Javadoc description in two lines"}
         };
     }
 
     @DataProvider
     public static Object[][] workUnitGroupName() {
         return new Object[][] {
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestArgumentContainer.class),
-                        TestArgumentContainer.GROUP_NAME},
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestExtraDocs.class),
-                        TestExtraDocs.GROUP_NAME}
+                {TestArgumentContainer.class, TestArgumentContainer.GROUP_NAME},
+                {TestExtraDocs.class, TestExtraDocs.GROUP_NAME}
         };
     }
 
     @DataProvider
     public static Object[][] workUnitGroupSummary() {
         return new Object[][] {
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestArgumentContainer.class),
-                        TestProgramGroup.DESCRIPTION},
-                {DocGenMocks.createDocWorkUnit(getNewInstance(), TestExtraDocs.class, DocGenMocks.mockClassDoc("Javadoc description")),
-                        ""}
+                {TestArgumentContainer.class, TestProgramGroup.DESCRIPTION},
+                {TestExtraDocs.class, Collections.EMPTY_MAP, ""},
         };
     }
 
     @Test(dataProvider = "workUnitDescriptions")
-    public void testGetDescription(final DocWorkUnit workUnit, final String expectedDescription) {
-        Assert.assertEquals(getNewInstance().getDescription(workUnit), expectedDescription);
+    public void testGetDescription(final Class<?> docWorkUnitClazz, final String javadocText, final Map<String, String> inlineTags,
+            final String expectedDescription) {
+        final DocWorkUnit mockWorkUnit = createMockWorkUnit(docWorkUnitClazz, javadocText, inlineTags);
+        Assert.assertEquals(getDefaultWorkUnitHandlerInstance().getDescription(mockWorkUnit), expectedDescription);
     }
 
     @Test(dataProvider = "workUnitSummary")
-    public void testGetSummaryForWorkUnit(final DocWorkUnit workUnit, final String expectedDescription) {
-        Assert.assertEquals(getNewInstance().getSummaryForWorkUnit(workUnit), expectedDescription);
+    public void testGetSummaryForWorkUnit(final Class<?> docWorkUnitClazz, final String javadocText, final Map<String, String> inlineTags,
+            final String expectedSummary) {
+        final DocWorkUnit mockWorkUnit = createMockWorkUnit(docWorkUnitClazz, javadocText, inlineTags);
+        Assert.assertEquals(getDefaultWorkUnitHandlerInstance().getSummaryForWorkUnit(mockWorkUnit), expectedSummary);
     }
 
     @Test(dataProvider = "workUnitGroupName")
-    public void testGetGroupNameForWorkUnit(final DocWorkUnit workUnit, final String expectedDescription) {
-        Assert.assertEquals(getNewInstance().getGroupNameForWorkUnit(workUnit), expectedDescription);
+    public void testGetGroupNameForWorkUnit(final Class<?> docWorkUnitClazz, final String expectedGroupName) {
+        final DocWorkUnit mockWorkUnit = DocGenMocks.createDocWorkUnit(
+                getDefaultWorkUnitHandlerInstance(), docWorkUnitClazz);
+        Assert.assertEquals(getDefaultWorkUnitHandlerInstance().getGroupNameForWorkUnit(mockWorkUnit), expectedGroupName);
     }
 
     @Test(dataProvider = "workUnitGroupSummary")
-    public void testGetGroupSummaryForWorkUnit(final DocWorkUnit workUnit, final String expectedDescription) {
-        Assert.assertEquals(getNewInstance().getGroupSummaryForWorkUnit(workUnit), expectedDescription);
+    public void testGetGroupSummaryForWorkUnit(final Class<?> docWorkUnitClazz, final String expectedGroupSummary) {
+        final DocWorkUnit mockWorkUnit = DocGenMocks.createDocWorkUnit(
+                getDefaultWorkUnitHandlerInstance(), docWorkUnitClazz);
+        Assert.assertEquals(getDefaultWorkUnitHandlerInstance().getGroupSummaryForWorkUnit(mockWorkUnit), expectedGroupSummary);
     }
 
-    private static DefaultDocWorkUnitHandler getNewInstance() {
+    private static DocWorkUnit createMockWorkUnit(final Class<?> docWorkUnitClazz, final String javadocText, final Map<String, String> inlineTags) {
+        final ClassDoc mockClassDoc = DocGenMocks.mockClassDoc(javadocText, inlineTags);
+        return DocGenMocks.createDocWorkUnit(getDefaultWorkUnitHandlerInstance(), docWorkUnitClazz, mockClassDoc);
+    }
+
+    private static DefaultDocWorkUnitHandler getDefaultWorkUnitHandlerInstance() {
         return new DefaultDocWorkUnitHandler(new HelpDoclet());
     }
 }
